fe4946c1b6fd40cd6700796a2e6d3ff2892abc1c
maxime.dharboulle <maxime.dharboulle@sequodia.com>
2021-01-09 14:27:54 +0100

    update 5

--- deploy_script.sh
+++
@@ -1,5 +0,0 @@
-cd tictactoe_rails_react 
-sudo docker-compose down
-git fetch origin
-git reset --hard origin/develop  &&  echo 'You are doing well'
-sudo docker-compose build && sudo docker-compose up -d
